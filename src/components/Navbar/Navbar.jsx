c1d66c5014637b20081a0634839cb458eed5dd62
Dibya Jyoti Routray <dibyajyotiroutray68@gmail.com>
2024-12-27 23:59:39 +0530

    some changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils.js";
@@ -46,5 +46,64 @@ export const Navbar = () => {
 };
 export default Navbar
 
+*/
 
+import React, { useState } from "react";
 
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils.js";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        DIBYA J ROUTRAY
+      </a>
+      <div className={styles.menu}>
+        <button
+          className={styles.menuBtn}
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle navigation menu"
+        >
+          <img
+            src={
+              menuOpen
+                ? getImageUrl("nav/closeIcon.png")
+                : getImageUrl("nav/menuIcon.png")
+            }
+            alt={menuOpen ? "Close menu" : "Open menu"}
+          />
+        </button>
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          role="menu"
+        >
+          <li>
+            <a href="#about" role="menuitem">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#experience" role="menuitem">
+              Experience
+            </a>
+          </li>
+          <li>
+            <a href="#projects" role="menuitem">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" role="menuitem">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
